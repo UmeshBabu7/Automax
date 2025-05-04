bf0cdb42a795272630493d80a742893d494cd01d
Umesh <umeshtamang129@gmail.com>
2025-05-04 10:04:33 +0545

    adding login view

--- src/automax/urls.py
+++ src/automax/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('main.urls'))
+    path('',include('main.urls')),
+    path('',include('users.urls'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
