8646422203f0a9b03853c941a43f9fe9ce720a3c
Umesh <umeshtamang129@gmail.com>
2025-05-08 08:34:31 +0545

    implementing header and logout features

--- src/users/views.py
+++ src/users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import authenticate,login
+from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.views import View
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -25,6 +26,12 @@ def login_view(request):
     elif request.method == 'GET':
         login_form = AuthenticationForm()
     return render(request, 'views/login.html', {'login_form': login_form})
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('main')
 
 
 class RegisterView(View):
