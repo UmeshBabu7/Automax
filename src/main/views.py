9f3f4606ca43a0d92175e439e7d0100b76028abc
Umesh <umeshtamang129@gmail.com>
2025-05-17 13:48:58 +0545

    custom forms

--- src/main/views.py
+++ src/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Listing
+from .forms import ListingForm
+from users.forms import LocationForm
 
 # Create your views here.
 
@@ -16,14 +18,16 @@ def home_view(request):
     }
     return render(request, 'views/home.html', context)
 
+
+
 @login_required
 def list_view(request):
     if request.method =='POST':
         pass
 
     elif request.method =='GET':
-        pass
-
-    return render(request,'views/list.html',{})
+        listing_form=ListingForm()
+        location_form=LocationForm()
+    return render(request,'views/list.html',{'listing_form':listing_form, 'location_form':location_form})
 
 
---
+++ src/users/forms.py
@@ -0,0 +1,16 @@
+from dataclasses import field
+from django import forms
+from django.contrib.auth.models import User
+from localflavor.us.forms import USZipCodeField
+
+from .models import Location
+
+
+class LocationForm(forms.ModelForm):
+
+    address_1 = forms.CharField(required=True)
+    zip_code = USZipCodeField(required=True)
+
+    class Meta:
+        model = Location
+        fields = {'address_1', 'address_2', 'city', 'state', 'zip_code'}
